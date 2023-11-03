211194639f09e9b2a7ee8fbd95b69674eedfc266
dianlyu1217 <lvdian1217gogo@gmail.com>
2023-11-03 13:59:13 -0700

    ok

--- cli.py
+++ cli.py
@@ -2,7 +2,7 @@ import logic
 import logging
 
 logging.basicConfig(level=logging.DEBUG,
-                    filename='./logs/output.log',
+                    filename='./logs/tictactoc.log',
                     datefmt='%Y/%m/%d %H:%M:%S',
                     format='%(asctime)s - %(levelname)s - %(filename)s- %(lineno)d  - %(message)s')
 logger = logging.getLogger('tictactoc')
